4accdc5e17f1351c35f1989cf040b1016f784129
Styzex <120598476+Styzex@users.noreply.github.com>
2024-07-13 12:54:55 +0200

    Fixing 404 Error 12

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: './vite/src', // Path to your source files inside the vite directory
   build: {
-    outDir: 'dist',
-  },
-  root: 'vite/src',
+    outDir: '../dist', // Relative path to the output directory, adjusted for the project root
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: '/index.html' // Specify the main entry point as index.html
+      }
+    }
+  }
 });
--- vite/src/main.js
+++ vite/src/main.js
@@ -1,3 +1,3 @@
-import '/style.css';
+import '/assets/style.css';
 
 // Any necessary initialization for the main page can be added here later
