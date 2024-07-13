e01e64771db8b912e2c4c405a5e7172357041a6d
Styzex <120598476+Styzex@users.noreply.github.com>
2024-07-13 11:34:40 +0200

    Fixing 404 error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  build: {
+    outDir: 'dist',
+  },
+  root: 'src',  // assuming your source files are in src directory
+});
--- vite/src/js/main.js
+++
@@ -1,3 +0,0 @@
-import '/vite/src/css/style.css';
-
-// Any necessary initialization for the main page can be added here later
